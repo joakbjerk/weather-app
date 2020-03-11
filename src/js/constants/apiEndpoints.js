882f00e2ab1d5e270da41f66888445783edb4ab3
joakim.bjerknes <joakim.bjerknes@PF0ZQ1NJ>
2020-03-11 14:36:49 +0100

    Removed cors-anywhere, as it seems like the locationforecast works as intended again

--- src/js/constants/apiEndpoints.js
+++ src/js/constants/apiEndpoints.js
@@ -1,7 +1,7 @@
 export const weatherIconApiEndpoint = 'https://api.met.no/weatherapi/weathericon/1.1?content_type=image%2Fpng&symbol=';
 
 export const locationForecastApiEndpoint = (latitude, longitude) =>
-  `https://cors-anywhere.herokuapp.com/https://api.met.no/weatherapi/locationforecast/1.9/.json?lat=${latitude}&lon=${longitude}`;
+  `https://api.met.no/weatherapi/locationforecast/1.9/.json?lat=${latitude}&lon=${longitude}`;
 
 export const toponymSearchApiEndpoint = name =>
   `https://ws.geonorge.no/SKWS3Index/ssr/json/sok?navn=${name}&epsgKode=4230`;
